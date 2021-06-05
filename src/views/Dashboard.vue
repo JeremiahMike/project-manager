8b46d42dd3dfdcc70ed45d71ea24c14a6ac53141
SpinachEater420 <michal.bilanin@gmail.com>
2021-06-05 20:07:31 +0200

    :sparkles: Dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,13 +6,11 @@
         <v-flex sm6 xs12 md6 lg3>
           <v-card class="ma-3">
             <v-list-item>
-              <v-list-item-avatar tile class="mt-n7">
-                <v-sheet color="green" width="80" height="80" elevation="10">
-                  <v-icon dark large>mdi-store</v-icon>
-                </v-sheet>
+              <v-list-item-avatar color="green" tile class="mt-n7">
+                <v-icon dark large>mdi-store</v-icon>
               </v-list-item-avatar>
               <v-list-item-content>
-                <div class="overline text-right">Article</div>
+                <div class="overline text-right">Articles</div>
                 <v-list-item-title class="headline mb-1 text-right"
                   >523614</v-list-item-title
                 >
@@ -21,42 +19,38 @@
             </v-list-item>
             <v-card-actions>
               <v-icon text class="ma-2">mdi-account</v-icon>
-              <div class="overline">Iyad</div>
+              <div class="overline">Lorem Ipsum</div>
             </v-card-actions>
           </v-card>
         </v-flex>
         <v-flex sm6 xs12 md6 lg3>
           <v-card class="ma-3">
             <v-list-item>
-              <v-list-item-avatar tile class="mt-n7">
-                <v-sheet color="#F44336" width="80" height="80" elevation="10">
-                  <v-icon dark large>mdi-youtube-subscription</v-icon>
-                </v-sheet>
+              <v-list-item-avatar color="red" tile class="mt-n7">
+                <v-icon dark large>mdi-youtube-subscription</v-icon>
               </v-list-item-avatar>
               <v-list-item-content>
-                <div class="overline text-right">Abonné</div>
+                <div class="overline text-right">Subscribers</div>
                 <v-list-item-title class="headline mb-1 text-right"
-                  >+700</v-list-item-title
+                  >700</v-list-item-title
                 >
                 <div><v-divider></v-divider></div>
               </v-list-item-content>
             </v-list-item>
             <v-card-actions>
-              <v-icon text class="ma-2">subscriptions</v-icon>
-              <div class="overline">Project Manager</div>
+              <v-icon text class="ma-2">mdi-youtube-subscription</v-icon>
+              <div class="overline">Lorem Ipsum</div>
             </v-card-actions>
           </v-card>
         </v-flex>
         <v-flex sm6 xs12 md6 lg3>
           <v-card class="ma-3">
             <v-list-item>
-              <v-list-item-avatar tile class="mt-n7">
-                <v-sheet color="#03A9F4" width="80" height="80" elevation="10">
-                  <v-icon dark large>mdi-cart</v-icon>
-                </v-sheet>
+              <v-list-item-avatar color="blue" tile class="mt-n7">
+                <v-icon dark large>mdi-cart</v-icon>
               </v-list-item-avatar>
               <v-list-item-content>
-                <div class="overline text-right">Shopping</div>
+                <div class="overline text-right">Sales</div>
                 <v-list-item-title class="headline mb-1 text-right"
                   >$34,245</v-list-item-title
                 >
@@ -65,20 +59,18 @@
             </v-list-item>
             <v-card-actions>
               <v-icon text class="ma-2">mdi-credit-card</v-icon>
-              <div class="overline">VISA Card</div>
+              <div class="overline">Lorem Ipsum</div>
             </v-card-actions>
           </v-card>
         </v-flex>
         <v-flex sm6 xs12 md6 lg3>
           <v-card class="ma-3">
             <v-list-item>
-              <v-list-item-avatar tile class="mt-n7">
-                <v-sheet color="#FFC107" width="80" height="80" elevation="10">
-                  <v-icon dark large>mdi-folder</v-icon>
-                </v-sheet>
+              <v-list-item-avatar color="yellow" tile class="mt-n7">
+                <v-icon dark large>mdi-folder</v-icon>
               </v-list-item-avatar>
               <v-list-item-content>
-                <div class="overline text-right">Folders shared</div>
+                <div class="overline text-right">Unfinished projects</div>
                 <v-list-item-title class="headline mb-1 text-right"
                   >1730</v-list-item-title
                 >
@@ -87,7 +79,7 @@
             </v-list-item>
             <v-card-actions>
               <v-icon text class="ma-2">mdi-folder</v-icon>
-              <div class="overline">Prodect</div>
+              <div class="overline">Lorem Ipsum</div>
             </v-card-actions>
           </v-card>
         </v-flex>
